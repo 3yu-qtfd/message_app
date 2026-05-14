a62074ce9827ec1a2abc00453af6e3fd0565f0bc
3yu-qtfd <118968276+3yu-qtfd@users.noreply.github.com>
2026-05-14 07:42:28 +0000

    データベース、テーブル追加

--- message_app.py
+++ message_app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request, redirect
+from database import init_db
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "GET":
+        
+        init_db()
+
     return render_template("index.html")
 
 app.run(host="0.0.0.0")
