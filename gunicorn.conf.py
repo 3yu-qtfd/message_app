900dcc9c565c25876489c91fe8c289ab32fd598d
3yu-qtfd_ec2 <118968276+3yu-qtfd@users.noreply.github.com>
2026-05-19 02:31:41 +0000

    Nginx追加、内部IPアドレス変更

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,6 @@
 import multiprocessing
 
-#bind = "127.0.0.1:8000"
-bind = "0.0.0.0:8000"
+#unix:/tmp/gunicorn.sock
+bind = "127.0.0.1:8000"
+#bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
